c2165c68c94260b07fffe696a0a6e5031a69fac0
zubair2685 <zubair34233@gmail.com>
2025-09-03 04:32:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -567,7 +567,7 @@ async def start(client, message):
         except:
             f_caption=f_caption
     if f_caption is None:
-        f_caption = f"{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files['file_name'].split()))}"
+        f_caption = files['file_name']
     if not await db.has_premium_access(message.from_user.id):
         if not await check_verification(client, message.from_user.id) and VERIFY == True:
             btn = [[
@@ -1404,3 +1404,4 @@ async def purge_requests(client, message):
             parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True
         )
+
